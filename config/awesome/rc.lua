26bd7aae9b6cd0023b0076e157e9d649967f2563
shanyouli <shanyouli6@gmail.com>
2020-09-02 16:08:55 +0800

    add Xresources 	new file:   config/X11/Xresources 	modified:   config/awesome/rc.lua

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -210,7 +210,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
+            -- mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
@@ -504,9 +504,9 @@ awful.rules.rules = {
       -- end,
       properties = { tag = "3", titlebars_enabled = false }
     },
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { name = "tmux" },
+      properties = { tag = "2", titlebars_enabled = false, fullscreen = true }
+    }
 }
 -- }}}
 -- naughty.notify({ title = "Achtung!", message = "You're idling", timeout = 0 })
