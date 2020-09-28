3933ffcef046f78d0835e94ef5869748dafb2c6f
shanyouli <shanyouli6@gmail.com>
2020-09-28 15:20:54 +0800

    add json 	modified:   config/doom/init.el

--- config/doom/init.el
+++ config/doom/init.el
@@ -126,6 +126,7 @@
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       json
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
