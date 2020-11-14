78898227b71a54dd89d2b0b04bdf0cef28d1f770
shanyou Li <shanyouli6@gmail.com>
2020-11-14 15:52:57 +0800

    fix my.path.xdg error 	modified:   hosts/personal.nix 	modified:   modules/default.nix

--- hosts/personal.nix
+++ hosts/personal.nix
@@ -42,10 +42,10 @@
   environment.variables = {
     # These are the defaults, but some applications are buggy when these lack
     # explicit values.
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_CACHE_HOME  = "$HOME/.cache";
-    XDG_DATA_HOME   = "$HOME/.local/share";
-    XDG_BIN_HOME    = "$HOME/.local/bin";
+    XDG_CONFIG_HOME = "${my.path.xdgConfig}";
+    XDG_CACHE_HOME  = "${my.path.xdgCache}";
+    XDG_DATA_HOME   = "${my.path.xdgData}";
+    XDG_BIN_HOME    = "${my.path.xdgBin}";
   };
 
   # Conform more programs to XDG conventions. The rest are handled by their
