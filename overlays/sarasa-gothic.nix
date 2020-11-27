01fa90b44597e510bb8c0eb2617318bb10274aec
shanyou Li <shanyouli6@gmail.com>
2020-11-27 17:19:00 +0800

    update flake

--- overlays/sarasa-gothic.nix
+++ overlays/sarasa-gothic.nix
@@ -1,10 +1,10 @@
-let version = "0.15.1";
+let version = "0.15.2";
 in
 final: prev: {
   sarasa-gothic = prev.sarasa-gothic.overrideAttrs (oldAttrs: rec {
     inherit version;
     name = "sarasa-gothic-${version}";
     url = "https://github.com/be5invis/Sarasa-Gothic/releases/download/v${version}/sarasa-gothic-ttc-${version}.7z";
-    sha256 = "sha256-crnuPsbHDGeEvB8YAuJtNMkephs89KabXQo7Av1ez54=";
+    sha256 = "1h8dlxdls3na8n71jhqvvb3vr7ri1myj7d346p4nvib31pqyl3an";
   });
 }
--- overlays/xst.nix
+++ overlays/xst.nix
@@ -9,7 +9,6 @@ final: prev: {
       repo = "xst";
       rev = "9837593355c15c8bdf607edefda9f72d32098ae3";
       sha256 = "sha256-nOJcOghtzFkl7B/4XeXptn2TdrGQ4QTKBo+t+9npxOA=";
-
     };
   });
 }
