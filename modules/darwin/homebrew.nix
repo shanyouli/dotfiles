382aa900dfd205460343a474a99d99eafa63e72e
Shanyou Li <shanyouli6@gmail.com>
2024-05-11 20:43:06 +0800

    add tencent-lemon brew package

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -145,6 +145,7 @@ in {
           (mkIf config.modules.shell.git.enGui "github") # github客户端
           (mkIf config.modules.browser.chrome.useBrew "google chrome")
           "command-x" # Cut files
+          "tencent-lemon" # 文件清理
         ]
         ++ optionals config.modules.shell.adb.enable [
           # "openmtp" # 目前不是很稳定
