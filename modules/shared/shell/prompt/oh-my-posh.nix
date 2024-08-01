49c883e7954b221fb8bef4e35d7e9b6f2d7ce87c
Shanyou Li <shanyouli6@gmail.com>
2024-08-01 20:27:19 +0800

    feat: adjust posh2k.omp.json

--- modules/shared/shell/prompt/oh-my-posh.nix
+++ modules/shared/shell/prompt/oh-my-posh.nix
@@ -9,20 +9,22 @@ with lib;
 with lib.my; let
   cfp = config.modules.shell.prompt;
   cfg = cfp.oh-my-posh;
+  package = pkgs.oh-my-posh;
+  formatFn = shell: "${package}/bin/oh-my-posh init ${shell} --config ${config.dotfiles.configDir}/oh-my-posh/posh2k.omp.json --print";
 in {
   options.modules.shell.prompt.oh-my-posh = {
     enable = mkEnableOption "Whether to use oh-my-posh";
   };
   config = mkIf cfg.enable {
-    environment.systemPackages = [pkgs.oh-my-posh];
+    environment.systemPackages = [package];
     programs.bash.interactiveShellInit = mkIf cfp.bash.enable ''
       if [[ $TERM != "dumb" && (-z $INSIDE_EMACS || $INSIDE_EMACS == "vterm") ]]; then
-        eval "$(oh-my-posh init bash --config ${config.dotfiles.configDir}/oh-my-posh/my-atomic.json --print)"
+        eval "$(${formatFn bash})"
       fi
     '';
-    modules.shell.nushell.cacheCmd = ["${pkgs.oh-my-posh}/bin/oh-my-posh init nu --config ${config.dotfiles.configDir}/oh-my-posh/my-atomic.json --print"];
+    modules.shell.nushell.cacheCmd = ["${formatFn nu}"];
     modules.shell.rcInit = lib.optionalString cfp.zsh.enable ''
-      _cache oh-my-posh init zsh --config ${config.dotfiles.configDir}/oh-my-posh/my-atomic.json --print
+      _cache ${formatFn zsh}
     '';
   };
 }
