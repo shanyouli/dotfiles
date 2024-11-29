38cc76cfc6ff377ba3b15a51e8793b9204c89bcc
Shanyou Li <shanyouli6@gmail.com>
2024-11-29 17:49:34 +0800

    bump: 2024(test1)

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
     # Principle inputs (updated by `nix run .#update`)
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    darwin-stable.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
-    nixos-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    darwin-stable.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
+    nixos-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
