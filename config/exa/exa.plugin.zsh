56b8bbaeba88c02b17ae3a254b36e4a914e8d1f6
Shanyou Li <shanyouli6@gmail.com>
2026-07-10 22:50:25 +0800

    chore: update exa plugin aliases for clarity and consistency

--- config/exa/exa.plugin.zsh
+++ config/exa/exa.plugin.zsh
@@ -1,7 +1,7 @@
-alias l="lla --git --sort-dirs-first --time-style=long-iso"
+alias l="lla --git --sort-dirs-first"
+alias ls="ls -g"
 alias lsi='l --icons'
 alias ll='ls -la'              # long, all
 alias llm='ll --sort=date' # list, long, sort by modification date
 alias tree="lla -t -f '.git'"
 alias treei="tree --icons"
-alias lS='eza -1' # one column by just names
