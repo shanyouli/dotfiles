ff45b232a63f105bf1e1f8ff8e9152838166eb00
Shanyou Li <shanyouli6@gmail.com>
2026-08-01 14:27:14 +0800

    feat: add clear tools module and fix ls alias

--- config/exa/exa.plugin.zsh
+++ config/exa/exa.plugin.zsh
@@ -1,5 +1,5 @@
 alias l="lla --git --sort-dirs-first"
-alias ls="ls -g"
+alias ls="l -g"
 alias lsi='l --icons'
 alias ll='ls -la'              # long, all
 alias llm='ll --sort=date' # list, long, sort by modification date
